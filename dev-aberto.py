852c435f69527155d238bd6527f23ac1bb806e02
Samuel <samuelgranato87@gmail.com>
2019-11-29 19:20:53 -0300

    added notepad when windows

--- dev-aberto.py
+++ dev-aberto.py
@@ -53,8 +53,12 @@ def edit_achievements(student_login):
     with open(f'students/{student_login}.temp', 'w') as f:
         f.write(json_achievements)
     
-    editor = os.getenv('EDITOR', default='vi')
-    
+    if('win' in sys.platform):
+        editor = os.getenv('EDITOR', default='notepad.exe')
+    else:
+        editor = os.getenv('EDITOR', default='vi')
+
+        
     valid_json = False
     while not valid_json:
         os.system(f'{editor} students/{student_login}.temp')
